2c0729ad93c73885479147c873d2e9efdc548924
Michael_Wang <wkj89@qq.com>
2020-09-25 23:12:46 +0800

    Update configure.sh

--- configure.sh
+++ configure.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
 echo $username
 echo $password
+echo $PORT
 export MINIO_ACCESS_KEY=zzzzzzzz
 export MINIO_SECRET_KEY=zzzzzzzz
-
-minio server /data --address "0.0.0.0:80" 
+echo "0.0.0.0:$PORT" 
+minio server /data --address "0.0.0.0:$PORT" 
